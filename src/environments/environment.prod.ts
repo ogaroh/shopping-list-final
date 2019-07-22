df61d17b8a88470b0d2ae2c29c075992dd495ffc
ogaroh <ogaroerick1998@gmail.com>
2019-07-22 10:08:31 +0300

    environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDpk7L-EgRrHd8Ddv5OKRRC8w9wyuGBpT4",
+    authDomain: "shopping-list-final.firebaseapp.com",
+    databaseURL: "https://shopping-list-final.firebaseio.com",
+    projectId: "shopping-list-final",
+    storageBucket: "",
+    messagingSenderId: "487610459140",
+    appId: "1:487610459140:web:0d829a445769f1de"
+  }
 };
