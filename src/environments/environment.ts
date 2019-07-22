3449560403459d7fbbdf55f9e76d156519e289c3
ogaroh <ogaroerick1998@gmail.com>
2019-07-22 09:56:11 +0300

    firebase environment setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDpk7L-EgRrHd8Ddv5OKRRC8w9wyuGBpT4",
+    authDomain: "shopping-list-final.firebaseapp.com",
+    databaseURL: "https://shopping-list-final.firebaseio.com",
+    projectId: "shopping-list-final",
+    storageBucket: "",
+    messagingSenderId: "487610459140",
+    appId: "1:487610459140:web:0d829a445769f1de"
+  }
 };
 
 /*
