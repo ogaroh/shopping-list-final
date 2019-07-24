1ac44bc96cff96979ee76cbc64cfc7af343e1b7e
ogaroh <ogaroerick1998@gmail.com>
2019-07-24 19:14:33 +0300

    sec

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDpk7L-EgRrHd8Ddv5OKRRC8w9wyuGBpT4",
-    authDomain: "shopping-list-final.firebaseapp.com",
-    databaseURL: "https://shopping-list-final.firebaseio.com",
-    projectId: "shopping-list-final",
-    storageBucket: "shopping-list-final.appspot.com",
-    messagingSenderId: "487610459140",
-    appId: "1:487610459140:web:0d829a445769f1de"
+    // apiKey: "AIzaSyDpk7L-EgRrHd8Ddv5OKRRC8w9wyuGBpT4",
+    // authDomain: "shopping-list-final.firebaseapp.com",
+    // databaseURL: "https://shopping-list-final.firebaseio.com",
+    // projectId: "shopping-list-final",
+    // storageBucket: "shopping-list-final.appspot.com",
+    // messagingSenderId: "487610459140",
+    // appId: "1:487610459140:web:0d829a445769f1de"
   }
 };
 
